dc0bde5e83ae826e6ae5fa7620dac80eb64fa305
Gabriel Mota <up202306287@up.pt>
2024-10-09 22:58:25 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function menu(){
+    if(document.getElementById("mySideBar").style.visibility == "visible") {
+        document.getElementById("mySideBar").style.visibility = "hidden"}
+    else document.getElementById("mySideBar").style.visibility = "visible";
+    return;
+}
